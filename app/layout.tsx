dfbcaa9b5f180021d4f5405ad4edad14f583c8d5
mikhail <mikhailabdillahtr@gmail.com>
2023-01-24 23:38:01 +0700

    feat: tailwindcss + postcss install

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import './globals.css'
+import { Poppins } from '@next/font/google'
+
+const font = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ['400', '500', '600', '700'],
+})
 
 export default function RootLayout({
   children,
@@ -6,13 +13,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={font.variable}>{children}</body>
     </html>
   )
 }
